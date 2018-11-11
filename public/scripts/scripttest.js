1d196dc48069185bc303840b2d64e2252b2bf810
scaldwell9 <ethan112096@gmail.com>
2018-11-11 00:58:53 -0500

    wow

--- public/scripts/scripttest.js
+++ public/scripts/scripttest.js
@@ -1,15 +1,36 @@
-console.log("Hello! I'm submitting data! Look at me go!!!");
-$(document).ready(function() {
-	$.getJSON('http://gd.geobytes.com/GetCityDetails?callback=?', function(data) {
-	  document.getElementById("ip").innerHTML=(JSON.stringify(data.geobytesipaddress, null, 2)).replace(/"/g,'');
-	});
-	console.log("I just successfully fetched your ip and now I am going to submit it to a database!");
 
+$(document).ready(function() {
+	console.log("Hello! I'm submitting data! Look at me go!!!");
+	//$.getJSON('http://gd.geobytes.com/GetCityDetails?callback=?', function(data) {
+	//  document.getElementById("ip").innerHTML=(JSON.stringify(data.geobytesipaddress, null, 2)).replace(/"/g,'');
+	//});
 });
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
-setCookie("c111","12",100);
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+setCookie("score1",0,100);
+setCookie("score2",0,100);
+console.log(getCookie("score2"));
+setCookie("score3",0,100);
+setCookie("score4",0,100);
+setCookie("score5",0,100);
+setCookie("score6",0,100);
--- public/scripts/scripttest3.js
+++ public/scripts/scripttest3.js
@@ -13,5 +13,28 @@ function getCookie(cname) {
     }
     return "";
 }
-var check = getCookie("cook");
-console.log(check);
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+var check = [];
+console.log(getCookie("score1"));
+console.log(getCookie("score2"));
+console.log(getCookie("score3"));
+console.log(getCookie("score4"));
+console.log(getCookie("score5"));
+console.log(getCookie("score6"));
+/*
+var check.push( getCookie("score1"));
+var check.push( getCookie("score2"));
+var check.push( getCookie("score3"));
+var check.push( getCookie("score4"));
+var check.push( getCookie("score5"));
+var check.push( getCookie("score6"));
+var i;
+for(i=0;i<check.length;i++){
+    console.log(check[i]);
+}
+*/
